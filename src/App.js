de13139ccb39419e4c5397b268e07b64529c78b9
kyh012412 <kyh0124.12@gmail.com>
2024-07-06 08:12:05 +0900

    정책 추가전 분리 확인

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>app</h1>
-      <a href="/temp-lotto">로또 a</a>
-      <Link to={'/temp-lotto'}>로또 link</Link>
+      <Routes>
+        <Route path="">
+          <h1>Toys</h1>
+          <a href="/toys">진짜 토이</a> <br />
+          <div>
+            <h2>이하 연습코드</h2>
+            <a href="/temp-lotto">로또 a</a> <br />
+            <a href="/myshop">샵</a> <br />
+            <a href="/iotest1">iotest1</a> <br />
+          </div>
+          <br />
+          {/* 다른 쪽으로 넘기기위해서 link태그사용 x */}
+          {/* <Link to={'/temp-lotto'}>로또 link</Link> */}
+          <button>히든</button>
+        </Route>
+        <Route path="/privacy" element={<>privacy</>}></Route>
+      </Routes>
     </div>
   );
 }
