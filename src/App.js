4ed6c5307ae122b0472f660a9b706a0dd4ad5816
kyh012412 <kyh0124.12@gmail.com>
2024-07-06 08:43:24 +0900

    link삭제 index추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route
-          path=""
+          index
           element={
             <>
               <h1>Toys</h1>
