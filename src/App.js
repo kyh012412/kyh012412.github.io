14c50602e9c3837400f9fad2e46097842289b3ce
kyh012412 <kyh0124.12@gmail.com>
2024-07-06 09:28:11 +0900

    Link 태그 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="" element={<Link to="/privacy">privacy</Link>}></Route>
         <Route
-          path=""
+          path="main"
           element={
             <>
-              <h1>Toys</h1>
-              <a href="/toys">진짜 토이</a> <br />
-              <div>
-                <h2>이하 연습코드</h2>
-                <a href="/temp-lotto">로또 a</a> <br />
-                <a href="/myshop">샵</a> <br />
-                <a href="/iotest1">iotest1</a> <br />
-              </div>
-              <br />
-              {/* 다른 쪽으로 넘기기위해서 link태그사용 x */}
-              {/* <Link to={'/temp-lotto'}>로또 link</Link> */}
-              <button>히든</button>
+              <>
+                <h1>Toys</h1>
+                <a href="/toys">진짜 토이</a> <br />
+                <div>
+                  <h2>이하 연습코드</h2>
+                  <a href="/temp-lotto">로또 a</a> <br />
+                  <a href="/myshop">샵</a> <br />
+                  <a href="/iotest1">iotest1</a> <br />
+                </div>
+                <br />
+                <button>히든</button>
+              </>
             </>
           }
         ></Route>
