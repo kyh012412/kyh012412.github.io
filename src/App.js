935d4a3160701ca76a5f3a71f17f67a377e35eed
kyh012412 <kyh0124.12@gmail.com>
2024-07-06 09:43:42 +0900

    route 내 path에는 /로 시작하지 않고 , Link에는 /path로 연결, homepoge에는 /로 끝나는상황

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             </>
           }
         ></Route>
-        <Route path="/privacy" element={<>privacy</>}></Route>
+        <Route path="privacy" element={<>privacy</>}></Route>
         <Route path="*" element={<>NotFound</>}></Route>
       </Routes>
     </div>
